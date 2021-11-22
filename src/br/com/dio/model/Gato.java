e63bb3372d6674e7a3314a7a3717ec5bca034e0e
vNaiPy <handrei.m.souza@hotmail.com>
2021-11-22 16:44:26 -0300

    first commit in intelij

---
+++ src/br/com/dio/model/Gato.java
@@ -0,0 +1,17 @@
+package br.com.dio.model;
+
+public class Gato {
+    private String nome;
+    private String cor;
+    private Integer idade;
+
+    public Gato() {
+
+    }
+
+    public Gato(String nome, String cor, Integer idade) {
+        this.nome = nome;
+        this.cor = cor;
+        this.idade = idade;
+    }
+}
