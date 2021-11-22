3635830fa2f03590a7de3c42770f128c35787105
vNaiPy <handrei.m.souza@hotmail.com>
2021-11-22 17:13:22 -0300

    Segundo commit com a inclusão do livro

--- src/br/com/dio/primeiroPrograma.java
+++ src/br/com/dio/primeiroPrograma.java
@@ -2,10 +2,49 @@ package br.com.dio;
 
 import br.com.dio.model.Gato;
 
+import java.util.SplittableRandom;
+
 public class primeiroPrograma {
     public static void main(String[] args) {
         Gato gato = new Gato();
 
         System.out.println(gato);
+
+        Livro livro1 = new Livro("O lar para crianças peculiares", 200);
+        System.out.println(livro1);
+    }
+}
+
+class Livro {
+    private String nome;
+    private Integer npag;
+
+    public Livro(String nome, Integer npag) {
+        this.nome = nome;
+        this.npag = npag;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNpag() {
+        return npag;
+    }
+
+    public void setNpag(Integer npag) {
+        this.npag = npag;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", npag=" + npag +
+                '}';
     }
 }
